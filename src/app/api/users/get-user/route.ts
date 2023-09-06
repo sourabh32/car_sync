34990dd4039f25a6df37f271f39c1d7fa5c3ae47
sourabh@32 <sourabhsharma181003@gmail.com>
2023-09-06 21:20:11 +0530

    something

--- src/app/api/users/get-user/route.ts
+++ src/app/api/users/get-user/route.ts
@@ -9,21 +9,21 @@ export async function GET(request: NextRequest) {
     try {
 
 
-    //     const token = request.cookies.get("token")?.value || '';
-    //     if(token === ''){
-    //         return NextResponse.json({
-    //             message: "User don't exist or logged out",
-    //         })
-    //     }
-    //     const decodedToken:any = jwt.verify(token, process.env.NEXT_PUBLIC_TOKEN_SECRET!);
-    //    console.log(decodedToken)
+        const token = request.cookies.get("token")?.value || '';
+        if(token === ''){
+            return NextResponse.json({
+                message: "User don't exist or logged out",
+            })
+        }
+        const decodedToken:any = jwt.verify(token, process.env.NEXT_PUBLIC_TOKEN_SECRET!);
+       console.log(decodedToken)
        
-       const decodedToken = {
-        id:"gdhdhdd",
-        username:"mangal",
-         role:"admin",
-        email:"mangal@gmail.com"
-       }
+    //    const decodedToken = {
+    //     id:"gdhdhdd",
+    //     username:"mangal",
+    //      role:"admin",
+    //     email:"mangal@gmail.com"
+    //    }
         
         return NextResponse.json({
             message: "User found",
