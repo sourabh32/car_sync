df52074d5f755696c13c2e80900814dce899b8f8
sourabh@32 <sourabhsharma181003@gmail.com>
2023-09-08 12:39:05 +0530

    hero image chnaged

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,9 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='flex flex-col gap-10 items-center justify-center'>
-        <div className='pt-12 '>
-        <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-bold">
-          Find, book, rent a car—quick and super easy!
+        <div className='pt-8 '>
+        <h1 className="2xl:text-[72px] sm:text-[64px] text-[50px] font-semibold">
+          Select car, pick time-slot, rent a car—quick and super easy!
         </h1>
 
         <p className="text-[27px] text-black-100 font-light mt-5">
@@ -14,8 +14,8 @@ const Hero = () => {
           process.
         </p>
         </div>
-        <div   >
-        <Image width={600} priority  height={600} alt="car" src={"/car.png"} />
+        <div  >
+        <Image width={1000} priority  height={600} alt="car" src={"/rolls.png"} />
         </div>
     </div>
   )
